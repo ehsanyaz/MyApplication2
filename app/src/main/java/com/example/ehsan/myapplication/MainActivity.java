b76e6967625004cecbb41b99d364ceecec80f885
Ehsan <yazdani.ehsan95@gmail.com>
2018-01-24 22:32:27 +0330

    q1

--- app/src/main/java/com/example/ehsan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ehsan/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ///salam ehsan
+        //salam senator
     }
 }
